7071640848bfef27edff316fa996b055a81dc606
VGiannac <vgiannac@mail.yu.edu>
2023-11-27 15:07:46 -0500

    Update notebook and __init__.py

---
+++ src/soccer_analysis/__init__.py
@@ -0,0 +1,6 @@
+## __init__.py
+
+
+from src.soccer_analysis.summary import DataSummary
+from src.soccer_analysis.eda import ExploratoryDataAnalysis
+from src.soccer_analysis.inference import Inference
