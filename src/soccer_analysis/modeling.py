f9b6a36ac6431cfa2f4be23a8dd26b5d5c55149b
VGiannac <vgiannac@mail.yu.edu>
2023-12-06 17:53:51 -0500

    Changing module

--- src/soccer_analysis/modeling.py
+++ src/soccer_analysis/modeling.py
@@ -9,37 +9,38 @@ class ModelingAnalysis:
         self.df = pd.read_csv(data_url)
         self.target = 'spi1'
 
+    def filter_chinese_league_2019(self):
+        print("Number of rows before filtering:", len(self.df))
+        
+        self.df = self.df[(self.df['league'] == 'Chinese Super League') & (self.df['season'] == 2019)]
+        self.df.reset_index(drop=True, inplace=True)
+        
+        print("Number of rows after filtering:", len(self.df))
+    
     def prepare_data(self):
         # Filter data for Chinese Super League in 2019
         self.filter_chinese_league_2019()
-
-        # One-hot encode categorical variables
-        self.df = pd.get_dummies(self.df, columns=['league'])
-
-        # Define features and target
-        self.features = ['league_id', 'team1', 'team2', 'spi2', 'prob1', 'prob2'] + list(self.df.filter(like='league_').columns)
-        X = self.df[self.features]
-        y = self.df[self.target]
-
-        # Ensure all columns are numeric
-        X = X.apply(pd.to_numeric, errors='coerce')
-
-        # Drop any rows with NaN values
-        X.dropna(inplace=True)
-        y = y[X.index]
-
-        return X, y
-
-    def prepare_data(self):
+    
         # One-hot encode categorical variables
         self.df = pd.get_dummies(self.df, columns=['league'])
     
         # Define features and target
-        self.features = ['league_id', 'team1', 'team2', 'spi2', 'prob1', 'prob2']
+        self.features = ['league_id', 'team1', 'team2', 'spi2', 'prob1', 'prob2'] + list(self.df.filter(like='league_').columns)
         X = self.df[self.features]
         y = self.df[self.target]
+    
+        # Filter out non-numeric columns
+        non_numeric_cols = X.select_dtypes(include=['object', 'bool']).columns
+        X.drop(non_numeric_cols, axis=1, inplace=True)
+    
+        # Ensure all columns are numeric
+        X = X.apply(pd.to_numeric, errors='coerce')
+    
+        # Drop any rows with NaN values
+        X.dropna(inplace=True)
+        y = y[X.index]
+    
         return X, y
-
     def split_data(self, X, y):
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         return X_train, X_test, y_train, y_test
