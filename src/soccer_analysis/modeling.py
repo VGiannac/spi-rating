86d807b179b0d2dac872418ffc721d94477bd484
VGiannac <vgiannac@mail.yu.edu>
2023-12-06 13:57:11 -0500

    Update modeling

--- src/soccer_analysis/modeling.py
+++ src/soccer_analysis/modeling.py
@@ -4,37 +4,38 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, classification_report
 
-    def ModelingAnalysis(data_url):
-        df = pd.read_csv(data_url)
-    
+class ModelingAnalysis:
+    def __init__(self, data_url):
+        self.data_url = data_url
+        self.df = pd.read_csv(data_url)
+        self.features = ['spi1', 'spi2', 'proj_score1', 'proj_score2']
+        self.target = 'outcome'
+        self.X_train, self.X_test, self.y_train, self.y_test = self.preprocess_data()
+
+    def preprocess_data(self):
         # Assuming you have a column 'outcome' with values 'win', 'draw', 'loss'
-        df['outcome'] = df['outcome'].map({'win': 1, 'draw': 0, 'loss': -1})
-    
-        features = ['spi1', 'spi2', 'proj_score1', 'proj_score2']
-        target = 'outcome'
-    
-        X = df[features]
-        y = df[target]
-    
+        self.df['outcome'] = self.df['outcome'].map({'win': 1, 'draw': 0, 'loss': -1})
+
+        X = self.df[self.features]
+        y = self.df[self.target]
+
         X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-    
+
         scaler = StandardScaler()
         X_train_scaled = scaler.fit_transform(X_train)
         X_test_scaled = scaler.transform(X_test)
-    
+
         return X_train_scaled, X_test_scaled, y_train, y_test
-    
-    def train_classifier(X_train, y_train):
+
+    def train_classifier(self):
         clf = RandomForestClassifier(random_state=42)
-        clf.fit(X_train, y_train)
+        clf.fit(self.X_train, self.y_train)
         return clf
-    
-    def evaluate_classifier(clf, X_test, y_test):
-        y_pred = clf.predict(X_test)
-    
-        accuracy = accuracy_score(y_test, y_pred)
+
+    def evaluate_classifier(self, clf):
+        y_pred = clf.predict(self.X_test)
+
+        accuracy = accuracy_score(self.y_test, y_pred)
         print(f'Accuracy: {accuracy:.2f}')
-    
-        print(classification_report(y_test, y_pred))
 
-
+        print(classification_report(self.y_test, y_pred))
