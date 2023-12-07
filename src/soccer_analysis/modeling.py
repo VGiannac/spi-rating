bca415224f7e356a5b4196a2d1bea5f0b6a8070d
VGiannac <vgiannac@mail.yu.edu>
2023-12-06 23:00:01 -0500

    doc strings added

--- src/soccer_analysis/modeling.py
+++ src/soccer_analysis/modeling.py
@@ -9,6 +9,20 @@ import matplotlib.pyplot as plt
 
 
 class ModelingAnalysis:
+    """
+        Initialize the ModelingAnalysis class.
+
+        Parameters
+        ----------
+        global_rankings_url : str
+            The URL or file path to the CSV containing global rankings data.
+        target_column : str, optional
+            The target column for prediction, default is 'spi'.
+        test_size : float, optional
+            The proportion of the dataset to include in the test split, default is 0.2.
+        random_state : int, optional
+            Controls the randomness of the data splitting, default is 42.
+        """
     def __init__(self, global_rankings_url, target_column='spi', test_size=0.2, random_state=42):
         self.df = self.load_data(global_rankings_url)
         self.target_column = target_column
@@ -19,10 +33,30 @@ class ModelingAnalysis:
         self.pipeline = self.create_pipeline()
 
     def load_data(self, url):
+        """
+        Load data from a CSV file.
+
+        Parameters
+        ----------
+        url : str
+            The URL or file path to the CSV file.
+
+        Returns
+        -------
+        pandas.DataFrame
+            The loaded DataFrame.
+        """
         return pd.read_csv(url)
 
     def prepare_data(self):
-    
+        """
+        Prepare the data for training and testing.
+
+        Returns
+        -------
+        tuple
+            A tuple containing the training and testing sets for features and target variable.
+        """
         columns_to_drop = [self.target_column, 'rank', 'prev_rank', 'name', 'league']
         existing_columns = set(self.df.columns)
         columns_to_drop = [col for col in columns_to_drop if col in existing_columns]
@@ -32,6 +66,14 @@ class ModelingAnalysis:
         return train_test_split(X, y, test_size=self.test_size, random_state=self.random_state)
 
     def create_pipeline(self):
+        """
+        Create a data processing and modeling pipeline.
+
+        Returns
+        -------
+        sklearn.pipeline.Pipeline
+            The constructed pipeline.
+        """
         numeric_features = self.X_train.select_dtypes(include=['int64', 'float64']).columns
         numeric_transformer = Pipeline(steps=[
             ('scaler', StandardScaler())
@@ -49,6 +91,9 @@ class ModelingAnalysis:
         return pipeline
 
     def train_model(self):
+         """
+        Train the model using the prepared data.
+        """
         self.pipeline.fit(self.X_train, self.y_train)
         self.model = self.pipeline.named_steps['regressor']
 
